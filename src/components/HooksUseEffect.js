7cc97423f79e302038ef481ab58a4ea252e2458b
AhsanRao <35622245+Ahsanrao237@users.noreply.github.com>
2021-03-09 05:57:47 +0500

    useEffect Hooks

--- src/components/HooksUseEffect.js
+++ src/components/HooksUseEffect.js
@@ -3,19 +3,29 @@ import React,{useEffect,useState} from 'react'
 export default function HooksUseEffect() {
 
     const [counter,setState]=useState(0)
+    const [counter1,setState1]=useState(0)
+    const [counter2,setState2]=useState(1)
 
-    const updateCounter=()=>{
+    const Increment=()=>{
         setState(counter+1);
+    }
+    const Decrement=()=>{
+        setState1(counter1-1);
+    }
+    const Twice=()=>{
+        setState2(counter2*2);
     }
 
     useEffect(()=>{
         console.log("Component Updated (UseEffect)")
-    })
+    },[counter,counter1]) //it means useEffect only run when only counter and counter1 updated
     return (
         <div>
             <p className="bg-warning font-weight-bold">Hooks UseEffect (useEffect Helps to implement LifeCycle in Functional Component)</p>
              
-            <button onClick={updateCounter}>update {counter}</button>
+            <button onClick={Increment}>Increment {counter}</button>
+            <button onClick={Decrement}>Decrement {counter1}</button>
+            <button onClick={Twice}>Twice {counter2}</button>
         </div>
     )
 }
